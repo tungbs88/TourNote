0a60bdf9e51b95268f564de6971d70fe4d72223f
Tung <tungbs88@gmail.com>
2019-04-09 23:41:32 +0700

    9.4.19

--- app/src/main/java/com/example/tournote/MainActivity.java
+++ app/src/main/java/com/example/tournote/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.tournote;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,14 +15,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "TourNote_MainActivity";
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle; // Điều khiển đóng/mở Drawer
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtEmptyNote = findViewById(R.id.activity_main_txt_empty);
-        ImageView imageView = findViewById(R.id.activity_main_img_note);
+        drawerLayout = findViewById(R.id.activity_main_drawer);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        // Hiện icon menu Drawer trên ActionBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
@@ -30,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Đồng bộ ActionBarDrawerToggle với Activity
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
         switch (item.getItemId()){
             case R.id.menu_item_search:
                 Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show();
@@ -43,5 +59,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Đồng bộ ActionBarDrawerToggle với Activity
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        // Sync the toggle state after onRestoreInstanceState has occurred
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
